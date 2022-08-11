6ed922f56a762ae8e46ce98d7be01fb0ead083f2
maisarahmusa <63534072+maisarahmusa@users.noreply.github.com>
2022-08-11 17:04:10 +0800

    RL-MODIFY_CONTROLLER

---
+++ app/Http/Controllers/PenilaianRekaBentukBangunanController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePenilaianRekaBentukBangunanRequest;
+use App\Http\Requests\UpdatePenilaianRekaBentukBangunanRequest;
+use App\Models\PenilaianRekaBentukBangunan;
+
+class PenilaianRekaBentukBangunanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePenilaianRekaBentukBangunanRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePenilaianRekaBentukBangunanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PenilaianRekaBentukBangunan  $penilaianRekaBentukBangunan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PenilaianRekaBentukBangunan $penilaianRekaBentukBangunan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PenilaianRekaBentukBangunan  $penilaianRekaBentukBangunan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PenilaianRekaBentukBangunan $penilaianRekaBentukBangunan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePenilaianRekaBentukBangunanRequest  $request
+     * @param  \App\Models\PenilaianRekaBentukBangunan  $penilaianRekaBentukBangunan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePenilaianRekaBentukBangunanRequest $request, PenilaianRekaBentukBangunan $penilaianRekaBentukBangunan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PenilaianRekaBentukBangunan  $penilaianRekaBentukBangunan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PenilaianRekaBentukBangunan $penilaianRekaBentukBangunan)
+    {
+        //
+    }
+}
