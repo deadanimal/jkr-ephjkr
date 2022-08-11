4aa550b22a03ac6cd8fc49d1ba82dd0992eabf06
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-08-11 15:46:17 +0800

    RL - add Pemudah Cara Model

---
+++ app/Http/Controllers/PemudahCaraController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePemudahCaraRequest;
+use App\Http\Requests\UpdatePemudahCaraRequest;
+use App\Models\PemudahCara;
+
+class PemudahCaraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePemudahCaraRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePemudahCaraRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PemudahCara  $pemudahCara
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PemudahCara $pemudahCara)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PemudahCara  $pemudahCara
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PemudahCara $pemudahCara)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePemudahCaraRequest  $request
+     * @param  \App\Models\PemudahCara  $pemudahCara
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePemudahCaraRequest $request, PemudahCara $pemudahCara)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PemudahCara  $pemudahCara
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PemudahCara $pemudahCara)
+    {
+        //
+    }
+}
