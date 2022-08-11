f22480fa6c704071fe907f101ad81c19b94feeee
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-08-11 16:50:47 +0800

    CL - clean code

--- resources/views/auth/daftar.blade.php
+++ resources/views/auth/daftar.blade.php
@@ -8,7 +8,75 @@
         </div>
     </div>
 
-    <div class="row">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <form action="/register" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >No. Kad Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="col-3">
+                        <label >Nama Pengguna:</label>
+                    </div>
+                    <div class="col-9 mb-2">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
